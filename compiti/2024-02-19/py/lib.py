dadc129030dca61631313382a1825566d5bc24f2
govoni <pietro.govoni@gmail.com>
2024-05-28 15:31:12 +0200

    compito del 19 febbraio 2024

---
+++ compiti/2024-02-19/py/lib.py
@@ -0,0 +1,25 @@
+import random
+from math import sqrt
+
+def rand_range (xMin, xMax) :
+    '''
+    generazione di un numero pseudo-casuale distribuito fra xMin ed xMax
+    '''
+    return xMin + random.random () * (xMax - xMin)
+
+def rand_TCL_ms (mean, sigma, N_sum = 10) :
+    '''
+    generazione di un numero pseudo-casuale 
+    con il metodo del teorema centrale del limite
+    note media e sigma della gaussiana
+    '''
+    y = 0.
+    delta = sqrt (3 * N_sum) * sigma
+    xMin = mean - delta
+    xMax = mean + delta
+    for i in range (N_sum) :
+        y = y + rand_range (xMin, xMax)
+    y /= N_sum
+    return y
+
+    
